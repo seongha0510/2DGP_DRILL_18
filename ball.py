0da4ad4c5a6838189729f5302be816f0cf1126b1
seongha0203 <kingsh0510@tukorea.ac.kr>
2025-12-02 15:44:58 +0900

    랜덤 볼 클래스 추가

--- ball.py
+++ ball.py
@@ -12,3 +12,27 @@ class Ball:
 
     def draw(self):
         self.image.draw(self.x, self.y)
+
+class RandomBall:
+    def __init__(self):
+        self.x = random.randint(300,500)
+        self.y = random.randint(600, 600)
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
+
+class Ball:
+    def __init__(self):
+        self.x = random.randint(600, 800)
+        self.y = random.randint(1200,1600)
+        self.image = load_image('ball21x21.png')
+
+    def update(self):
+        pass
+
+    def draw(self):
+        self.image.draw(self.x, self.y)
